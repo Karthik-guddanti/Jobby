85e7b0b8c67a7de41fdef5b7dc14a0738a57db91
karthik <karthikguddanti25@gmailcom>
2025-06-17 18:31:08 +0530

    commiting second time

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -13,8 +13,8 @@ const Sidebar = ({ onTypeChange, onSalaryChange }) => {
     <div>
       <div className="card">
         <img src="" alt="profile" />
-        <h3>Karthik</h3>
-        <p>Lead Frontend Developer</p>
+        <h3>Manichandana</h3>
+        <b>Lead Frontend Developer</b>
       </div>
       <hr />
       <h2>Type of Employment</h2>
@@ -31,6 +31,8 @@ const Sidebar = ({ onTypeChange, onSalaryChange }) => {
         <label><input type="radio" name="salary" value="20" onChange={handleSalaryChange} />20LPA and above</label>
         <label><input type="radio" name="salary" value="30" onChange={handleSalaryChange} />30LPA and above</label>
         <label><input type="radio" name="salary" value="40" onChange={handleSalaryChange} />40LPA and above</label>
+        <label><input type="radio" name="salary" value="0" onChange={handleSalaryChange} />0LPA and above</label>
+        
       </div>
     </div>
   );
